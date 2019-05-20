5862441913b2d861d9f887d350e6bb5bda452d55
Lucas Lima <lucass.limaa@outlook.com.br>
2019-05-20 11:26:20 -0300

    alteração para colocar online

--- conectaBanco.php
+++ conectaBanco.php
@@ -3,9 +3,9 @@
  * Conexao com banco de dados, utilizando PDO.
  */
 $servidor = "localhost";
-$usuario_bd = "root";
-$senha_bd = "123Def456#";
-$banco = "db_savemoney";
+$usuario_bd = "id9596498_root";
+$senha_bd = "123654789";
+$banco = "id9596498_db_savemoney";
 
 $con = new PDO("mysql:host=$servidor;dbname=$banco", $usuario_bd, $senha_bd);
 ?>
